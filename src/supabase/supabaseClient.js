eb473c7d069d2ad7ade1c9b0c74d4ce79777298e
Ravi kumar <ravik09783@gmail.com>
2026-03-01 13:06:05 +0530

    hard code for env

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace with your actual values from Supabase dashboard
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://opckeukdnpytblnkfuwf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wY2tldWtkbnB5dGJsbmtmdXdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3MTg0MDksImV4cCI6MjA2MjI5NDQwOX0.xb1B2wvvXN-j7n2lcBdfcQ6cpRgTNwqMlS1rah2Jdvg'
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
